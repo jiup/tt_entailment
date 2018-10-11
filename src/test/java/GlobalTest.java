fe762dbf5a070f9043fc63355b920f3067045617
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2018-10-11 16:51:29 -0400

    Initial commit

---
+++ src/test/java/GlobalTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author Jiupeng Zhang
+ * @since 10/11/2018
+ */
+public class GlobalTest {
+    @Test
+    public void test() {
+        System.out.println("Hello world!");
+    }
+}
